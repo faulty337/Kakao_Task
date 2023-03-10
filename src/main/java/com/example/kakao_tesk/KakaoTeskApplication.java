9aa15a408a2e81d06525cd8c257bc51623fb7432
faulty337 <faulty337@gmail.com>
2023-03-10 10:30:02 +0900

    Feat : 기본 틀 추가

---
+++ src/main/java/com/example/kakao_tesk/KakaoTeskApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakao_tesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoTeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoTeskApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/h2/schema.sql
@@ -0,0 +1,43 @@
+CREATE TABLE Menu
+(
+    id       BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name     VARCHAR(20) NOT NULL,
+    price    INT         NOT NULL,
+    status   TINYINT(1) DEFAULT 1,
+    category VARCHAR(20)
+);
+
+
+CREATE TABLE Users
+(
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    total_point BIGINT DEFAULT 0
+);
+
+CREATE TABLE Point
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    amount     BIGINT   DEFAULT 0,
+    created_at DATETIME DEFAULT NOW(),
+    user_id    BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users (id)
+);
+
+CREATE TABLE Order
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    price      BIGINT   DEFAULT 0,
+    created_at DATETIME DEFAULT NOW(),
+    user_id    BIGINT NOT NULL,
+    menu_id    BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users (id),
+    FOREIGN KEY (menu_id) REFERENCES Menu (id)
+);
+
+CREATE TABLE Rank
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    price     BIGINT   DEFAULT 0,
+    name VARCHAR(2) DEFAULT NOW(),
+    category    BIGINT NOT NULL
+);
