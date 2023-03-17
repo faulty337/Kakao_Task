344396025c22600a43c742d8d99d7023ac83e4ed
faulty337 <faulty337@gmail.com>
2023-03-17 17:29:38 +0900

    Feat : 에러 형태 추가

--- src/main/java/com/example/kakao_tesk/exception/ErrorCode.java
+++ src/main/java/com/example/kakao_tesk/exception/ErrorCode.java
@@ -8,7 +8,9 @@ public enum ErrorCode {
     NOT_FOUND_USER("존재하지 않는 유저 입니다.", 403),
     SHORTAGE_POINT("포인트가 부족합니다", 400),
     BAD_MENU_ID("잘못된 주문 번호입니다.", 400),
-    NOT_FOUND_MENU("존재하지 않는 메뉴입니다.", 400);
+    NOT_FOUND_MENU("존재하지 않는 메뉴입니다.", 400),
+    NOT_FOUND_LOCK("Lock을 획득하지 못했습니다.", 500),
+    SERVER_ERROR("서버에러입니다. 서버관리자에게 문의주세요", 500);
 
 
     private final String message;
